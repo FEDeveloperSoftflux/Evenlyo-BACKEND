918723ba252b5e8f7fe9737669b06ebac69ce793
mtalal517 <mtalalqureshi5@gmail.com>
2025-08-06 12:35:37 +0500

    Categories Fix

--- routes/categories.js
+++ routes/categories.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 
-
 // Public routes (no authentication required)
 router.get('/', categoryController.getCategories);
+router.get('/all', categoryController.getAllCategories);
 router.get('/:id', categoryController.getCategoryById);
 
 module.exports = router;
