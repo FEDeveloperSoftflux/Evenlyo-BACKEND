4ca3102ebb3cb832361d68575c70d2594b3425ef
mtalal517 <mtalalqureshi5@gmail.com>
2025-08-29 14:56:02 +0500

    CORS

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
+  'http://localhost:5173',
   'http://localhost:3001',
   'http://localhost:5000',
   'http://127.0.0.1:5000',
