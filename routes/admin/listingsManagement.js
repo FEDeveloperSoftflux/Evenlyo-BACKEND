622b338b24a3bbc5ee8e7dd50b728eeca5eab070
mtalal517 <mtalalqureshi5@gmail.com>
2025-10-10 16:14:44 +0500

    service fix +  sub category

--- routes/admin/listingsManagement.js
+++ routes/admin/listingsManagement.js
@@ -1,7 +1,7 @@
 // TOGGLE subcategory status by subcategory id
 const express = require('express');
 const router = express.Router();
-const {getAllListingManagementData, getSubCategoriesByMainCategory,toggleSubCategoryStatus,createSubCategory}= require('../../controllers/admin/listingManagement');
+const {getAllListingManagementData, getSubCategoriesByMainCategory,toggleSubCategoryStatus,createSubCategory, editSubCategory}= require('../../controllers/admin/listingManagement');
 
 // GET all listing management data in one response
 router.get('/stats', getAllListingManagementData);
@@ -13,5 +13,7 @@ router.get('/subcategories/:mainCategoryId', getSubCategoriesByMainCategory);
 router.patch('/subcategories/:subCategoryId/toggle', toggleSubCategoryStatus);
 
 router.post('/create/subcategories', createSubCategory);
+
+router.put('/edit/subcategories/:subCategoryId', editSubCategory);
 
 module.exports = router;
